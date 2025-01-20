ce3e310de46631d5df477ab6346e50de801e6f0f
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2025-01-20 11:56:32 -0400

    Fixups

--- tests/Playground.php
+++ tests/Playground.php
@@ -1,7 +1,8 @@
 <?php
+
 use AACS\AuthTokenRSA;
 
-include_once (__DIR__.'/../vendor/autoload.php');
-include_once (__DIR__.'/AuthTokenRSATest.php');
+include_once(__DIR__ . '/../vendor/autoload.php');
+include_once(__DIR__ . '/AuthTokenRSATest.php');
 
 echo AuthTokenRSA::Create(AuthTokenRSATest::$testPrivKey, 1, 'fooorg');
